1f0f023a610c12994f3c9270eaa2b8cc2676e9dc
Michael Matloka <dev@twixes.com>
2021-01-06 22:25:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# PostHog x Segment
+
+This is the code of PostHog's integration with Segment. While more limited than our own libraries, this allows you to send events to all your destinations with just one Segment library call.
+
+## Questions?
+
+### [Join our Slack community.](https://join.slack.com/t/posthogusers/shared_invite/enQtOTY0MzU5NjAwMDY3LTc2MWQ0OTZlNjhkODk3ZDI3NDVjMDE1YjgxY2I4ZjI4MzJhZmVmNjJkN2NmMGJmMzc2N2U3Yjc3ZjI5NGFlZDQ)
+
+We're here to help you with anything PostHog!
---
+++ index.js
@@ -0,0 +1,112 @@
+/**
+ * @param {SpecTrack} event The track event
+ * @param {Object.<string, any>} settings Custom settings
+ * @return any
+ */
+async function onTrack(event, settings) {
+    const baseUrl = settings.postHogInstance || 'https://app.posthog.com'
+    const endpoint = new URL(`${baseUrl}/capture/`)
+    endpoint.searchParams.set('ts', event.timestamp)
+    if (event.properties && event.properties.url) {
+        event.properties['$current_url'] = event.properties.url
+        delete event.properties.url
+    }
+    const res = await fetch(endpoint, {
+        body: JSON.stringify({
+            ...event,
+            api_key: settings.apiKey,
+            properties: {
+                ...event.properties,
+                distinct_id: event.userId || event.anonymousId,
+            },
+        }),
+        headers: new Headers({
+            'Content-Type': 'application/json',
+        }),
+        method: 'post',
+    })
+
+    return await res.json()
+}
+
+/**
+ * @param {SpecIdentify} event The identify event
+ * @param {Object.<string, any>} settings Custom settings
+ * @return any
+ */
+async function onIdentify(event, settings) {
+    if (!event.userId) {
+        throw new InvalidEventPayload('userId is required')
+    }
+    return await onTrack(
+        {
+            ...event,
+            event: '$identify',
+            $anon_distinct_id: event.anonymousId,
+            $set: event.traits,
+        },
+        settings
+    )
+}
+
+/**
+ * @param {SpecGroup} event The group event
+ * @param {Object.<string, any>} settings Custom settings
+ * @return any
+ */
+async function onGroup(event, settings) {
+    throw new EventNotSupported('on groups is not supported')
+}
+
+/**
+ * @param {SpecPage} event The page event
+ * @param {Object.<string, any>} settings Custom settings
+ * @return any
+ */
+async function onPage(event, settings) {
+    return await onTrack(
+        {
+            event: '$pageview',
+            ...event,
+        },
+        settings
+    )
+}
+
+/**
+ * @param {SpecAlias} event The alias event
+ * @param {Object.<string, any>} settings Custom settings
+ * @return any
+ */
+async function onAlias(event, settings) {
+    return await onTrack(
+        {
+            ...event,
+            event: '$create_alias',
+            properties: {
+                alias: event.previousId,
+                distinct_id: event.userId,
+            },
+        },
+        settings
+    )
+}
+
+/**
+ * @param {SpecScreen} event The screen event
+ * @param {Object.<string, any>} settings Custom settings
+ * @return any
+ */
+async function onScreen(event, settings) {
+    return await onTrack(
+        {
+            ...event,
+            event: '$screen',
+            properties: {
+                ...event.properties,
+                $screen_name: event.name,
+            },
+        },
+        settings
+    )
+}
